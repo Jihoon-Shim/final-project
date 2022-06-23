f5e1f23005076001fcc5931edbdf8cbaa00d09b8
yeonho-oh <ohdusho@gmail.com>
2022-06-23 15:33:57 +0900

    1차 pagination

--- finalOnion/src/main/java/org/kosta/myproject/vo/MemberVO.java
+++ finalOnion/src/main/java/org/kosta/myproject/vo/MemberVO.java
@@ -2,8 +2,12 @@ package org.kosta.myproject.vo;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MemberVO implements Serializable{
 	private static final long serialVersionUID = 6440047762418162093L;
 	private String memberId;
@@ -14,5 +18,6 @@ public class MemberVO implements Serializable{
 	private int onionPoint;
 	private String memberNickname;
 	private String memberPicture;
+	private TempVO tempVO;
 	private int enabled;
 }
