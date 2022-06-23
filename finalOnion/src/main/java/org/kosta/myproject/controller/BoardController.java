894c8f7507e672db384865c20fd335023d941b5b
yeonho-oh <ohdusho@gmail.com>
2022-06-23 15:58:12 +0900

    삽니다 오류 수정1

--- finalOnion/src/main/java/org/kosta/myproject/controller/BoardController.java
+++ finalOnion/src/main/java/org/kosta/myproject/controller/BoardController.java
@@ -77,6 +77,62 @@ public class BoardController {
 		request.setAttribute("url", "board/list.jsp");
 		return "board/buylist.html";
 	}
+	@RequestMapping("/board/salelist")
+	public String salelist(Model model) {
+		ArrayList<TradingBoardVO> list = new ArrayList<TradingBoardVO>();
+		//클라이언트로부터 페이지번호를 전달받는다. Pagination(dao.getTotalPostCount(),nowPage);
+		String pageNo =(String) model.getAttribute("pageNo");
+		Pagination pagination = null;
+		if(pageNo==null) {
+			pagination = new Pagination(boardService.getTotalPostCount());
+		}else {
+			pagination=new Pagination(boardService.getTotalPostCount(),Integer.parseInt(pageNo));
+		}
+		//list.jsp에서 페이징처리를 하기위해 Pagination객체를 공유한다.
+		list = boardService.orderByDate(pagination);
+		model.addAttribute("pagination",pagination);
+		model.addAttribute("list",list);
+		return "board/buylist.html";
+	}
+	@RequestMapping("/board/ordersaleList")
+	public String ordersaleList(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		HttpSession session = request.getSession(false);
+		String sort = request.getParameter("sort1");
+		String sorton = request.getParameter("sorton");
+		if(session.getAttribute("sorting")==null) {
+			if(sorton!=null) {
+				session.setAttribute("sorting", sort);
+			}else{
+				session.setAttribute("sorting", "date");
+			}
+		}
+		if(sorton!=null) {
+			session.setAttribute("sorting", sort);
+		}
+		String sort1 =(String) session.getAttribute("sorting");
+		ArrayList<TradingBoardVO> list = new ArrayList<TradingBoardVO>();
+		//클라이언트로부터 페이지번호를 전달받는다. Pagination(dao.getTotalPostCount(),nowPage);
+		String pageNo = request.getParameter("pageNo");
+		Pagination pagination = null;
+		if(pageNo==null) {
+			pagination = new Pagination(boardService.getTotalPostCount());
+		}else {
+			pagination=new Pagination(boardService.getTotalPostCount(),Integer.parseInt(pageNo));
+		}
+		//list.jsp에서 페이징처리를 하기위해 Pagination객체를 공유한다.
+		
+		if(sort1.equals("temp")) {
+			list = boardService.orderByTemp(pagination);
+		}else if(sort1.equals("price")) {
+			list = boardService.orderByPrice(pagination);
+		}else{
+			list = boardService.orderByDate(pagination);
+		}
+		request.setAttribute("pagination", pagination);
+		request.setAttribute("list", list);
+		request.setAttribute("url", "board/list.jsp");
+		return "board/buylist.html";
+	}
 	
 	@RequestMapping("/board/chat")
 	public String salelist() {
