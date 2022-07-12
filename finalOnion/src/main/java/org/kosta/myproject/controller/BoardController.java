38152860ce9e0e4531afda67179832d4706dda8b
Jihoon-Shim <tlawnsdl37@gmail.com>
2022-07-12 16:25:58 +0900

    하이

--- finalOnion/src/main/java/org/kosta/myproject/controller/BoardController.java
+++ finalOnion/src/main/java/org/kosta/myproject/controller/BoardController.java
@@ -2,9 +2,7 @@ package org.kosta.myproject.controller;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
